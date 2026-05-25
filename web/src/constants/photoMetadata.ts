7cd443ecd9ec84a79523cf561b58f6821ba1e475
thein3rovert <danielolaibi@gmail.com>
2026-05-25 11:15:13 +0100

    refactor: extract photo metadata constants to separate file

---
+++ web/src/constants/photoMetadata.ts
@@ -0,0 +1,56 @@
+export const COMMON_CAMERAS = [
+  'FUJIFILM X-E4',
+  'FUJIFILM X-T5',
+  'FUJIFILM X100VI',
+  'CANON EOS R5',
+  'SONY A7 IV',
+  'NIKON Z8',
+]
+
+export const COMMON_LENSES = [
+  '23MM F/2',
+  '35MM F/2',
+  '50MM F/2',
+  '56MM F/1.2',
+  '16-55MM F/2.8',
+  '18-55MM F/2.8-4',
+]
+
+export const COMMON_APERTURES = [
+  'F/1.2',
+  'F/1.4',
+  'F/1.8',
+  'F/2',
+  'F/2.8',
+  'F/4',
+  'F/5.6',
+  'F/8',
+]
+
+export const COMMON_SHUTTERS = [
+  '1/4000S',
+  '1/2000S',
+  '1/1000S',
+  '1/500S',
+  '1/250S',
+  '1/125S',
+  '1/60S',
+  '1/30S',
+]
+
+export const COMMON_ISOS = [
+  '100',
+  '200',
+  '400',
+  '800',
+  '1600',
+  '3200',
+  '6400',
+]
+
+export const COMMON_LOCATIONS = [
+  'London, UK',
+  'Tokyo, Japan',
+  'New York, USA',
+  'Paris, France',
+]
